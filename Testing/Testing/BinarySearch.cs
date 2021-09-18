7fc3fbee276ee72a01510260c5f2a8be21dd20f5
Yatish <ykasaraneni3@gatech.edu>
2021-09-18 17:23:09 -0400

    Practise BinarySearch

---
+++ Testing/Testing/BinarySearch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    public class BinarySearch
+    {
+        public bool BinarySearchImpl(int[] sortedInput, int right, int left, int searchTerm)
+        {
+            if (right >= left)
+            {
+                int mid = left + ((right - left) / 2);
+                if (sortedInput[mid] == searchTerm)
+                {
+                    return true;
+                }
+
+                if (sortedInput[mid] > searchTerm)
+                {
+                    return BinarySearchImpl(sortedInput, mid - 1, left, searchTerm);
+                }
+
+                return BinarySearchImpl(sortedInput, right, mid + 1, searchTerm);
+            }
+
+
+            return false;
+        }
+    }
+}
